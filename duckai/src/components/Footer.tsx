a7a6ac9a4f0c40e1ae54ea12b2f90ece2d69d20d
cdeng65 <cdeng65@gatech.edu>
2023-04-26 14:32:02 -0400

    font

--- duckai/src/components/Footer.tsx
+++ duckai/src/components/Footer.tsx
@@ -1,14 +1,24 @@
-// src/components/Footer.tsx
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; 
+import { faGithub, faTwitter, faDiscord } from '@fortawesome/free-brands-svg-icons';
 
 const Footer: React.FC = () => {
   return (
-    <nav>
-      <a href="mailto:email@example.com">Email</a>
-      <a href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a>
-      <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
-      <a href="https://discord.com" target="_blank" rel="noopener noreferrer">Discord</a>
-    </nav>
+    <div>
+      <a href="mailto:duckai.ml@gmail.com ">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+      <a href="https://github.com/TheDuckAI" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a href="https://twitter.com/TheDuckAI" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faTwitter} />
+      </a>
+      <a href="https://discord.gg/myhu5pAEX7" target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon icon={faDiscord} />
+      </a>
+    </div>
   );
 };
 
--- duckai/src/pages/_app.tsx
+++ duckai/src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Cormorant_Garamond } from "@next/font/google"
+
+export const cormorantGaramond = Cormorant_Garamond({
+  subsets: ['cyrillic'],
+  weight: '300'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={cormorantGaramond.className}>
+      <Component {...pageProps} />
+    </main>
+  )
+  
 }
