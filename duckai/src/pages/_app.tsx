70b6bfba8559f0a2e5385287131e6846c25686ce
cdeng65 <cdeng65@gatech.edu>
2023-05-09 23:48:47 -0400

    draft blog

--- duckai/src/pages/_app.tsx
+++ duckai/src/pages/_app.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Cormorant_Garamond } from "@next/font/google";
 import { AnimatePresence } from "framer-motion";
+
 
 export const cormorantGaramond = Cormorant_Garamond({
   subsets: ["cyrillic"],
---
+++ duckai/src/pages/blog.tsx
@@ -0,0 +1,60 @@
+import React from "react"
+import PostCard from "@/components/Postcard"
+import { InferGetStaticPropsType } from "next"
+import fs from "fs"
+import { serialize } from "next-mdx-remote/serialize"
+import path from "path"
+import { PostPreview } from "@/types/posts"
+import Layout from "@/components/Layout"
+
+export default function Home({ postPreviews }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <Layout>
+        <div className={"row"}>
+      {postPreviews.map((postPreview, i) => {
+        return (
+          <div className={"col-6 mb-4"} key={i}>
+            <PostCard postPreview={postPreview} />
+          </div>
+        )
+      })}
+    </div>
+
+    </Layout>
+    
+  )
+}
+
+export async function getStaticProps() {
+  // get all MDX files
+  const postFilePaths = fs.readdirSync("_posts").filter((postFilePath) => {
+    return path.extname(postFilePath).toLowerCase() === ".mdx"
+  })
+
+  const postPreviews: PostPreview[] = []
+
+  // read the frontmatter for each file
+  for (const postFilePath of postFilePaths) {
+    const postFile = fs.readFileSync(`_posts/${postFilePath}`, "utf8")
+
+    // serialize the MDX content to a React-compatible format
+    // and parse the frontmatter
+    const serializedPost = await serialize(postFile, {
+      parseFrontmatter: true,
+    })
+
+    postPreviews.push({
+      ...serializedPost.frontmatter,
+      // add the slug to the frontmatter info
+      slug: postFilePath.replace(".mdx", ""),
+    } as PostPreview)
+  }
+
+  return {
+    props: {
+      postPreviews: postPreviews,
+    },
+    // enable ISR
+    revalidate: 60,
+  }
+}
