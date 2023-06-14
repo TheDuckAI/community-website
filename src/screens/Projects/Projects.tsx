2a1e2e6187e84e7bfca9da8798627988ee177e75
tomohiro-sawada <tomosawada98@gmail.com>
2023-06-13 23:00:02 -0400

    Updated project descriptions

--- src/screens/Projects/Projects.tsx
+++ src/screens/Projects/Projects.tsx
@@ -9,37 +9,44 @@ const projects = [
   {
     name: "Advanced Reasoning Benchmark (ARB) Project (previously: DAAB)",
     description:
-      "advanced prompting methods, measure performance of LLMs to solve academic problems",
+      "(#benchmark) advanced prompting methods, measure performance of LLMs to solve academic problems",
     status: "In Progress",
     githubLink: "https://github.com/TheDuckAI/DUCK-datasets",
   },
   {
-    name: "Multimodal Dataset",
+    name: "Multimodal Dataset Project",
     description:
-      "build dataset for training multimodal agents",
+      "(#multimodal) build dataset for training multimodal agents",
     status: "In Progress",
     githubLink: "https://github.com/TheDuckAI/multimodal-computer-dataset",
   },
   {
-    name: "Minerva OCR",
+    name: "Minerva-OCR Project",
     description:
-      "An OCR model of quality comparable to proprietary OCR solutions that can generate bounding boxes around any word that appears on an image and label them with the corresponding word.",
+      "(#minerva-ocr) An OCR model of quality comparable to proprietary OCR solutions that can generate bounding boxes around any word that appears on an image and label them with the corresponding word.",
     status: "In Progress",
     githubLink: "https://github.com/TheDuckAI/donut-ocr",
   },
   {
-    name: "Learning Machine Learning",
+    name: "Exploring video pretraining for sample efficient RL",
     description:
-      "bring people up to speed in SoTA machine learning",
-    status: "In Progress",
-    githubLink: "https://github.com/TheDuckAI/",
-  },
-  {
-    name: "Video RL",
-    description:
-      "improve pre training of video models for sample efficient RL agents",
+      "(#video-rl) Improve pre training of video models for sample efficient RL agents",
     status: "In Progress",
     githubLink: "https://github.com/TheDuckAI/video-rl",
+  },
+  {
+    name: "Non-English GPTs and better datasets and tokenizers",
+    description:
+      "(#polylingual) Build higher-quality non-English datasets and tokenizers and finetune non-English GPTs out of a pretrained English GPT",
+    status: "In Progress",
+    githubLink: "https://github.com/TheDuckAI/polylingual",
+  },
+  {
+    name: "Learning Machine Learning",
+    description:
+      "(#learning) bring people up to speed in SoTA machine learning",
+    status: "In Progress",
+    // githubLink: "https://github.com/TheDuckAI/",
   },
 ];
 
