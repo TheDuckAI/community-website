0ce77a4d4c67568e40eea1574f8e8954f95c44ca
cdeng65 <cdeng65@gatech.edu>
2023-06-27 18:53:46 -0400

    google meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from 'next/head'
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Cormorant_Garamond } from "@next/font/google";
@@ -11,10 +12,16 @@ export const cormorantGaramond = Cormorant_Garamond({
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
+    <>
+    <Head>
+      <meta name="google-site-verification" content="G-3MhyXayiPClo-PvC5XOHYF5jvgpC1YVXWMv7darRQ" />
+    </Head>
     <AnimatePresence mode="wait" initial={false}>
       <main className={cormorantGaramond.className}>
         <Component {...pageProps} key={router.asPath} />
       </main>
     </AnimatePresence>
+    </>
+    
   );
 }
